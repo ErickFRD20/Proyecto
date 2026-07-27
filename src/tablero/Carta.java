988e0a8771298e793203a146041397b469486394
ErickFRD <erick.reyesd1416@gmail.com>
2026-07-27 15:03:29 -0600

    Se crea la clase carta
    Se crea la clase carta dentro del paquete tablero, la clase contiene sus variables definidas y los metodos los cuales se encuentran en proceso

---
+++ src/tablero/Carta.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tablero;
+
+/**
+ *
+ * @author UTN
+ */
+public class Carta {
+    private int imagen;
+    private boolean visible;
+    private boolean encontrada;
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public boolean isEncontrada() {
+        return encontrada;
+    }
+
+    public Carta(int imagen) {
+        this.imagen = imagen;
+        this.visible = false;
+        this.encontrada = false;
+    }
+    
+    public void mostrar(){
+        
+    }
+    
+    public void ocultar(){
+        
+    }
+    
+    public void encontrar(){
+        
+    }
+    
+    public void reiniciar(){
+        
+    }
+}
