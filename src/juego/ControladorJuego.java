9753669b8f24f6ea3c869ca0759fd08d01cbae2c
Marco Ramirez Quiros <maaramirezqu@est.utn.ac.cr>
2026-07-30 17:24:47 -0600

    Controlador Juego casi completo
    La clase controlador juego casi terminada faltan agregar algunos metodos y metodos get y corregir errores

---
+++ src/juego/ControladorJuego.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juego;
+
+import tablero.Nivel;
+import tablero.Tablero;
+
+/**
+ *
+ * @author mr117
+ */
+public class ControladorJuego {
+    private Juego juego;
+    
+    public ControladorJuego(){
+        juego = new Juego();
+    }    
+    public Juego getJuego() {
+        return juego;
+    }
+    public Tablero getTablero() {
+    return juego.getTablero();
+    }
+    public int getPuntaje(){
+        return juego.getPuntaje();
+    }
+    public int getIntentos(){
+        return juego.getIntentos();
+    }
+    
+    
+    public void iniciarJuego(){
+        juego.iniciarPartida();
+    }
+    public void reiniciarJuego(){
+        juego.reiniciarPartida();
+    }
+    public void cambiarNivel(Nivel nivel){
+        juego.cambiarNivel(nivel);
+    }
+    public void seleccionarCarta(int fila, int columna){
+        juego.seleccionarCarta(fila, columna);
+    }
+    
+}
